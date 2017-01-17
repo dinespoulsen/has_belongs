61e353405641f5d54a0b47a12284c6be68e5dc7a
Kim Wilson <k.wilson541@gmail.com>
2017-01-17 16:54:11 +0000

    search class wrapped in module

--- lib/has_belongs.rb
+++ lib/has_belongs.rb
@@ -1,4 +1,5 @@
 require "has_belongs/version"
+require "has_belongs/search"
 require "thor"
 
 
@@ -8,15 +9,18 @@ module HasBelongs
 
     desc "install", "should run a command line task"
     def install
+
       create_file("spec/sandbox/test.txt")
     end
 
     desc "migrate ASSOCIATION", "should run a migration command passing in one parameter"
-    def migrate(association)
-      system("touch #{association}")
-      system("bin/rake db:migrate")
-      system("bin/rake db:schema:load")
-      puts "has_belongs ran successfully"
+    def migrate
+    	search = HasBelongs::Search.new
+    	output = search.generate_migration("spec/test_models")
+    	systemt(output)
+      # system("bin/rake db:migrate")
+      # system("bin/rake db:schema:load")
+      # puts "has_belongs ran successfully"
     end
 
   end
